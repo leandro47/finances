8c4aac3fd3a4363dfd7f5879e5a5420cc5ddc217
LEANDRO DA SILVA <32754449+leandro47@users.noreply.github.com>
2020-12-22 22:03:47 -0300

    add scripts of template in the project

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -1,12 +1,24 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class User extends BaseController
 {
-	public function index()
+	public function __construct()
 	{
-		return view('user/login');
+		$this->datas['titlePage'] = 'Login';
 	}
 
-    //--------------------------------------------------------------------
+
+	public function index()
+	{
+		echo view('includes/header', $this->datas);
+		echo view('includes/menu', $this->datas);
+		echo view('dashboard/welcome', $this->datas);
+		echo view('includes/footer', $this->datas);
+		echo view('includes/scripts', $this->datas);
+	}
+
+	//--------------------------------------------------------------------
 
 }
---
+++ app/Views/includes/scripts.php
@@ -0,0 +1,16 @@
+  <!--   Core JS Files   -->
+  <script src="<?= base_url() ?>/assets/js/core/jquery.min.js"></script>
+  <script src="<?= base_url() ?>/assets/js/core/popper.min.js"></script>
+  <script src="<?= base_url() ?>/assets/js/core/bootstrap.min.js"></script>
+  <script src="<?= base_url() ?>/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+  <!-- Chart JS -->
+  <script src="<?= base_url() ?>/assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="<?= base_url() ?>/assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Black Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="<?= base_url() ?>/assets/js/black-dashboard.min.js?v=1.0.0"></script>
+  <!-- Black Dashboard DEMO methods, don't include it in your project! -->
+  <script src="<?= base_url() ?>/assets/demo/demo.js"></script>
+  <script src="<?= base_url('assets/custom/js/dashboard_welcome.js') ?>"></script>
